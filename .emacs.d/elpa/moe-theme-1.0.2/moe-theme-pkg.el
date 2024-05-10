06b144d5b8e9f4c948df28933be893731849bba0
josejosepreso <josehihat@gmail.com>
2024-05-09 23:54:53 -0600

    init.el

---
+++ .emacs.d/elpa/moe-theme-1.0.2/moe-theme-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from moe-theme.el  -*- no-byte-compile: t -*-
+(define-package "moe-theme" "1.0.2" "A colorful eye-candy theme. Moe, moe, kyun!" 'nil :commit "52102d23f5a20d6349abc645c19bd04370773241" :authors '(("kuanyui" . "azazabc123@gmail.com")) :maintainer '("kuanyui" . "azazabc123@gmail.com") :keywords '("themes") :url "https://github.com/kuanyui/moe-theme.el")
