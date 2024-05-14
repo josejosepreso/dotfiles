1633abbfa908a16a225d72327001e1b59190abe2
josejosepreso <josehihat@gmail.com>
2024-05-13 22:26:28 -0600

    initfile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,13 +1,9 @@
 (setq inhibit-startup-message t)
-
 (tooltip-mode -1)
-
 ;; Visible bell
 (setq visible-bell t)
-
+(tool-bar-mode -1)
 (menu-bar-mode -1)
-
-(require 'dracula-theme)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -15,15 +11,17 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("58c996beb973f7e988ee4fd21c367b7a5bbdb0622ddfbbd112672a7b4e3d3b81" "a68624bd5c4bec879ee59cd3039531b6229766a8b8ed0e79eef2642f14dbda32" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
+   '("2ccdb4796d3238dd0794f7869750fb0e81fe4f9212f9528cfd4f41da0c78cf25" "821c37a78c8ddf7d0e70f0a7ca44d96255da54e613aa82ff861fe5942d3f1efc" "afeb7b07dbc1a4cfadb24f3ef6c8cf5e63051bf76411779f03a0fe3aadc07768" "58c996beb973f7e988ee4fd21c367b7a5bbdb0622ddfbbd112672a7b4e3d3b81" "a68624bd5c4bec879ee59cd3039531b6229766a8b8ed0e79eef2642f14dbda32" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
  '(package-selected-packages
-   '(haskell-mode evil emms emmet-mode moe-theme dracula-theme)))
+   '(tron-legacy-theme exwm haskell-mode evil emms emmet-mode moe-theme dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+(load-theme 'tron-legacy)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
