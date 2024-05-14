cd54b37ed7e34267767bb55adbebb9e40373039f
josejosepreso <josehihat@gmail.com>
2024-05-14 13:39:01 -0600

    Update dotfiles

---
+++ .config/doom/custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("77f1e155387d355fbbb3b382a28da41cc709b2a1cc71e7ede03ee5c1859468d2" "8d3d935ad6797516f375b97b0d36961d4741ed84563284267bc42a1936a79830" "fe6b6d4be494bd23c360115ab703985b6366ab766800c9d9b960b0da113123e9" "8c7e832be864674c220f9a9361c851917a93f921fedb7717b1b5ece47690c098" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(dmenu kuronami-theme ayu-theme moe-theme gnu-elpa-keyring-update exwm emmet-mode dracula-theme @)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
