75435f1cf541080c523e3ae3c94e9bb4ac3d28f9
josejosepreso <josehihat@gmail.com>
2024-05-23 20:40:33 -0600

    xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -2,21 +2,22 @@ import XMonad
 import XMonad.Config.Desktop
 import XMonad.Util.EZConfig
 import qualified XMonad.Actions.CycleWS as CWS -- Last visited workspace
-import qualified XMonad.StackSet as W -- Set master
-import XMonad.Layout.Spacing -- Window spacing
-import XMonad.Actions.CycleWS -- Cycle through workspaces
+import qualified XMonad.StackSet as W          -- Set master
+import XMonad.Layout.Spacing                   -- Window spacing
+import XMonad.Actions.CycleWS                  -- Cycle through workspaces
+import XMonad.Actions.GridSelect
 
 baseConfig = desktopConfig {
                modMask = mod4Mask
              , focusedBorderColor = "#bbc5ff"
              , borderWidth        = 2
-             }
+            }
 
 main :: IO ()
 main = xmonad $ baseConfig {
                   startupHook = startupHook baseConfig <+> idHook
-                 , layoutHook = spacingWithEdge 3 $ layoutHook baseConfig
-                }
+                , layoutHook = spacingWithEdge 3 $ layoutHook baseConfig
+               }
                 `additionalKeysP` myKeys
 
 myKeys :: [(String, X ())]
@@ -42,6 +43,8 @@ myKeys = [
                  ---
                , ("M-c", spawn "urxvt -e mocp")
                , ("M-v", spawn "urxvt -e ranger")
+               , ("M-m", spawn "mplay")
+               , ("M-e", spawn "emacsclient -c -a ''")
 
                  --- Swap the focused window and the master window
                , ("M-S-m", windows W.swapMaster)
@@ -52,4 +55,36 @@ myKeys = [
                  --- Cycle through non empty workspaces
                , ("M-S-l", moveTo Next (Not emptyWS))
                , ("M-S-h", moveTo Prev (Not emptyWS))
+
+                 --- Go to workspace prompt
+               , ("M-S-g", goToSelected $ mygridConfig myColorizer)
+               , ("M-S-b", bringSelected $ mygridConfig myColorizer)
  ]
+
+       
+------------------------------------------------------------------------
+---GRID SELECT
+------------------------------------------------------------------------
+myColorizer :: Window -> Bool -> X (String, String)
+myColorizer = colorRangeFromClassName
+                  (0x31,0x2e,0x39) -- lowest inactive bg
+                  (0x31,0x2e,0x39) -- highest inactive bg
+                  (0x61,0x57,0x72) -- active bg
+                  (0xc0,0xa7,0x9a) -- inactive fg
+                  (0xff,0xff,0xff) -- active fg
+                  
+-- gridSelect menu layout
+mygridConfig colorizer = (buildDefaultGSConfig myColorizer)
+    { gs_cellheight   = 30
+    , gs_cellwidth    = 200
+    , gs_cellpadding  = 8
+    , gs_originFractX = 0.5
+    , gs_originFractY = 0.5
+    --, gs_font         = myFont
+    }
+    
+spawnSelected' :: [(String, String)] -> X ()
+spawnSelected' lst = gridselect conf lst >>= flip whenJust spawn
+    where conf = def
+
+
