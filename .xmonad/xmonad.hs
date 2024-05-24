ce715c2db90782d49b766c4b0df1e96d10301887
josejosepreso <josehihat@gmail.com>
2024-05-23 20:45:17 -0600

    xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,7 +5,7 @@ import qualified XMonad.Actions.CycleWS as CWS -- Last visited workspace
 import qualified XMonad.StackSet as W          -- Set master
 import XMonad.Layout.Spacing                   -- Window spacing
 import XMonad.Actions.CycleWS                  -- Cycle through workspaces
-import XMonad.Actions.GridSelect
+import XMonad.Actions.GridSelect               -- Grid prompt
 
 baseConfig = desktopConfig {
                modMask = mod4Mask
@@ -57,34 +57,6 @@ myKeys = [
                , ("M-S-h", moveTo Prev (Not emptyWS))
 
                  --- Go to workspace prompt
-               , ("M-S-g", goToSelected $ mygridConfig myColorizer)
-               , ("M-S-b", bringSelected $ mygridConfig myColorizer)
+               , ("M-S-g", goToSelected def)
+               , ("M-S-b", bringSelected def)
  ]
-
-       
-------------------------------------------------------------------------
----GRID SELECT
-------------------------------------------------------------------------
-myColorizer :: Window -> Bool -> X (String, String)
-myColorizer = colorRangeFromClassName
-                  (0x31,0x2e,0x39) -- lowest inactive bg
-                  (0x31,0x2e,0x39) -- highest inactive bg
-                  (0x61,0x57,0x72) -- active bg
-                  (0xc0,0xa7,0x9a) -- inactive fg
-                  (0xff,0xff,0xff) -- active fg
-                  
--- gridSelect menu layout
-mygridConfig colorizer = (buildDefaultGSConfig myColorizer)
-    { gs_cellheight   = 30
-    , gs_cellwidth    = 200
-    , gs_cellpadding  = 8
-    , gs_originFractX = 0.5
-    , gs_originFractY = 0.5
-    --, gs_font         = myFont
-    }
-    
-spawnSelected' :: [(String, String)] -> X ()
-spawnSelected' lst = gridselect conf lst >>= flip whenJust spawn
-    where conf = def
-
-
