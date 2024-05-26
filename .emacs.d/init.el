8dc19d7d4847e52a121de0a579f301ee55337535
josejosepreso <josehihat@gmail.com>
2024-05-26 15:50:47 -0600

    initfile

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,7 +4,7 @@
 (setq visible-bell t)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
-;;(scroll-bar-mode -1)
+(scroll-bar-mode -1)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -13,10 +13,9 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("774218d0781ca9aad07888de412eac35b7920bafc10ecc014ecf493d7a74b310" "77f1e155387d355fbbb3b382a28da41cc709b2a1cc71e7ede03ee5c1859468d2" "2ccdb4796d3238dd0794f7869750fb0e81fe4f9212f9528cfd4f41da0c78cf25" "821c37a78c8ddf7d0e70f0a7ca44d96255da54e613aa82ff861fe5942d3f1efc" "afeb7b07dbc1a4cfadb24f3ef6c8cf5e63051bf76411779f03a0fe3aadc07768" "58c996beb973f7e988ee4fd21c367b7a5bbdb0622ddfbbd112672a7b4e3d3b81" "a68624bd5c4bec879ee59cd3039531b6229766a8b8ed0e79eef2642f14dbda32" "603a831e0f2e466480cdc633ba37a0b1ae3c3e9a4e90183833bc4def3421a961" default))
- '(elfeed-feeds
-   '("https://www.reddit.com/r/virgin.rss"))
+ '(elfeed-feeds '("https://www.reddit.com/r/virgin.rss"))
  '(package-selected-packages
-   '(elfeed dmenu timu-caribbean-theme kuronami-theme php-mode tron-legacy-theme exwm haskell-mode evil emms emmet-mode moe-theme dracula-theme)))
+   '(rust-mode elfeed dmenu timu-caribbean-theme kuronami-theme php-mode tron-legacy-theme exwm haskell-mode evil emms emmet-mode moe-theme dracula-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -25,7 +24,7 @@
  )
 
 (set-face-attribute 'default nil :height 120)
-(load-theme 'timu-caribbean)
+(load-theme 'tron-legacy)
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
@@ -46,5 +45,13 @@
   (package-install 'evil))
 
 ;; Enable Evil
-;;(require 'evil)
-;;(evil-mode 1)
+(require 'evil)
+(evil-mode 1)
+
+;; Write backups to ~/.emacs.d/backup/
+(setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
+      backup-by-copying      t  ; Don't de-link hard links
+      version-control        t  ; Use version numbers on backups
+      delete-old-versions    t  ; Automatically delete excess backups:
+      kept-new-versions      20 ; how many of the newest versions to keep
+      kept-old-versions      5) ; and how many of the old
