18e47df0aacc2050e0bbdaa8dc005ab1872ae5d8
josejosepreso <josehihat@gmail.com>
2024-05-26 20:06:40 -0600

    xmonad

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,16 +5,19 @@ import qualified XMonad.Actions.CycleWS as CWS -- Last visited workspace
 import qualified XMonad.StackSet as W          -- Set master
 import XMonad.Actions.CycleWS                  -- Cycle through workspaces
 import XMonad.Actions.GridSelect               -- Grid prompt
+import XMonad.Actions.NoBorders
+import XMonad.Hooks.WindowSwallowing
 
 baseConfig = desktopConfig {
                modMask = mod4Mask
-             -- , focusedBorderColor = "#bbc5ff"
+             , focusedBorderColor = "#bbc5ff"
              , borderWidth        = 1
             }
 
 main :: IO ()
 main = xmonad $ baseConfig {
                   startupHook = startupHook baseConfig <+> idHook
+                , handleEventHook = swallowEventHook (className =? "URxvt") (return True) 
                }
                 `additionalKeysP` myKeys
 
@@ -40,10 +43,11 @@ myKeys = [
                
                  ---
                -- , ("M-c", spawn "urxvt -e mocp")
-               , ("M-c", spawn "urxvt -e ncmpcpp")
+               , ("M-c", spawn "urxvt -e mocp")
                , ("M-v", spawn "urxvt -e ranger")
                , ("M-m", spawn "mplay")
                , ("M-e", spawn "emacsclient -c -a ''")
+               , ("M-S-y", spawn "ytfzf -D")
 
                  --- Swap the focused window and the master window
                , ("M-S-m", windows W.swapMaster)
@@ -52,10 +56,13 @@ myKeys = [
                , ("M-<Tab>", CWS.toggleWS)
 
                  --- Cycle through non empty workspaces
-               , ("M-S-l", moveTo Next (Not emptyWS))
-               , ("M-S-h", moveTo Prev (Not emptyWS))
+               -- , ("M-S-l", moveTo Next (Not emptyWS))
+               , ("M-S-<Space>", moveTo Next (Not emptyWS))
+               -- , ("M-S-h", moveTo Prev (Not emptyWS))
 
                  --- Go to workspace prompt
                , ("M-S-g", goToSelected def)
                , ("M-S-b", bringSelected def)
+
+               , ("M-S-=", withFocused toggleBorder)
  ]
