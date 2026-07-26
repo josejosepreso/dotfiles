3140b179dd9c06031ea8ebfa6722ebde83ce3141
jose <jose@deb>
2026-07-25 22:17:20 -0600

    add doom config

---
+++ .config/doom/config.el
@@ -0,0 +1,113 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+;; (setq user-full-name "John Doe"
+;;       user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-symbol-font' -- for symbols
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+;; (setq doom-theme 'ef-elea-dark)
+(setq doom-theme 'doom-miramare)
+;; (setq doom-theme 'doom-sourcerer)
+;; (setq doom-theme 'kanagawa-dragon)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers 'relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; change `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `with-eval-after-load' block, otherwise Doom's defaults may override your
+;; settings. E.g.
+;;
+;;   (with-eval-after-load 'PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look them up).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+(setq doom-font (font-spec :family "Fira Code Nerd Font" :size 17))
+;; (setq doom-font (font-spec :family "Iosevka" :size 18))
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; and `package-pinned-packages`. Most users will not need or want to do this.
+;;(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(require 'multiple-cursors)
+(global-set-key (kbd "C-c C-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
+(global-set-key (kbd "C-d") 'scroll-up-command)
+(global-set-key (kbd "C-u") 'scroll-down-command)
+
+(require 'lsp-java)
+(add-hook 'java-mode-hook #'lsp)
+
+(setq lsp-java-vmargs
+      (list
+       "-noverify"
+       "-Xmx1G"
+       "-XX:+UseG1GC"
+       "-XX:+UseStringDeduplication"
+       "-javaagent:/home/jose/.m2/repository/org/projectlombok/lombok/1.18.44/lombok-1.18.44.jar"))
+
+(use-package move-text
+  :ensure t
+  :config
+  (move-text-default-bindings))
---
+++ .config/doom/custom.el
@@ -0,0 +1,36 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("5d9a9d99fc64805e672b8de16550197dc3c1b94674981bce3251ae5608f26114"
+     "6efc2371dc77a14f7dc6618d836e960ca4e64ee24424cfb85481d69716f0df58"
+     "26bd2e38c276535badb651063ed796a540caf3beaf004b1f62f89ab50341fdb2"
+     "47e5541e0d1ef82fb4a5191b23c0bcad90af74c690400aac4b8e2233b48d0dd3"
+     "6f80a4696a6afcaf6bb4e92be879432ca8566f042f9c0e6cbeff1f5eb6280bc3"
+     "5c8a1b64431e03387348270f50470f64e28dfae0084d33108c33a81c1e126ad6"
+     "c44617ae58f5005e0d736f0c6213fbb20b385adc8d0e8e4c7f2eb32bb90e3531"
+     "720838034f1dd3b3da66f6bd4d053ee67c93a747b219d1c546c41c4e425daf93"
+     "4c92d278dc295b63daf817d668523d442058d6c90728958dc92b6bc976fffd96"
+     "5ebfb530161061f8a6048fb49ddf1fe7d213862631f29f47e9884d15a421d7c1"
+     "f1e8339b04aef8f145dd4782d03499d9d716fdc0361319411ac2efc603249326"
+     "c20728f5c0cb50972b50c929b004a7496d3f2e2ded387bf870f89da25793bb44"
+     "daa27dcbe26a280a9425ee90dc7458d85bd540482b93e9fa94d4f43327128077"
+     "d2ab3d4f005a9ad4fb789a8f65606c72f30ce9d281a9e42da55f7f4b9ef5bfc6"
+     "0d2c5679b6d087686dcfd4d7e57ed8e8aedcccc7f1a478cd69704c02e4ee36fe"
+     "1c6cfdfd3e34a05bcd835ec0c4a293d5b48547b64baba257d8fd9c64c1bfb48b"
+     "eb4310ea38f9e59b43bd9eb0ec72cb0e95cf3f6602a66d88d591c8c66831e834"
+     "dc5a696b209b62c8c76daec0d3185262ec4fe07d46c79e62a6c960d61557a1db"
+     "a12cc418a4adedebe9e6647d5762ffbe7d16399e4d1362bffb0afba60244dc2c"
+     "9589caa8c1cae63496dfdcb1fe586f77041fe89467567712ee8682eae6f1d545"
+     "bf4ab01ac7cbd5bff3d3f43d9988b77f8adcbfae0c7a2fd7db5800649ac10210"
+     "38b43b865e2be4fe80a53d945218318d0075c5e01ddf102e9bec6e90d57e2134" default))
+ '(package-selected-packages '(haskell-mode move-text)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
